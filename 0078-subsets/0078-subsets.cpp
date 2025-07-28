de987dddc104bb19c7db610a3ae06f132aa804d6
Rahul Gupta <rahulgupta79777@gmail.com>
2025-07-28 13:49:23 +0530

    Time: 0 ms (100%), Space: 10 MB (60.59%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -5,22 +5,6 @@ public:
         vector<vector<int>> yo;
         ok(yo,0,hello,nums);
         int p=0;
-        vector<vector<int>> okk;
-        // for(int i=1;i<yo.size();i++){
-        //     sort(yo[i].begin(),yo[i].end());
-        // }
-        // okk.push_back(yo[0]);
-        // sort(yo.begin(), yo.end(), [](const vector<int>& a, const vector<int>& b) {
-        //     if(a.size()!=b.size()){
-        //         return a.size()<b.size();
-        //     }
-        //     return a<b;
-        // });
-        // for (int i = 1; i < yo.size(); ++i) {
-        //     if (yo[i] != yo[i - 1]) {
-        //         okk.push_back(yo[i]);
-        //     }
-        // }
         return yo;
     }
     void ok(vector<vector<int>>& arr,int index,vector<int>& hello,vector<int>& arr1){
