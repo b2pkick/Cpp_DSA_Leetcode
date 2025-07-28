07b56f9b00e22fa5842c63400c4c65cd3282465c
Rahul Gupta <rahulgupta79777@gmail.com>
2025-07-28 17:53:52 +0530

    Time: 3 ms (67.94%), Space: 14.4 MB (33.43%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        int count=0;
-        unordered_map<int,int> mp;
-        for(auto it:nums){
-            mp[it]++;
-        }
         vector<int> p;
         for(int i=0;i<nums.size();i++){
-            count=0;
-            for(auto it:mp){
-                if(it.first<nums[i]){
-                    count+=it.second;
-                }
+            p.push_back(nums[i]);
+        }
+        sort(p.begin(),p.end());
+        unordered_map<int,int> mp;
+        for(int i=0;i<p.size();i++){
+            if(mp.find(p[i])!=mp.end()) continue;
+            else{
+                mp[p[i]]=i;
             }
-            p.push_back(count);
+        }
+        for(int i=0;i<nums.size();i++){
+            p[i]=mp[nums[i]];
         }
         return p;
     }
