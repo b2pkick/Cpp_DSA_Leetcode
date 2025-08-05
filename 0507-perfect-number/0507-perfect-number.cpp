10af77df8eb08b71453038bc22fe13339f61ecfb
Rahul Gupta <rahulgupta79777@gmail.com>
2025-08-05 18:40:21 +0530

    Time: 254 ms (56.87%), Space: 7.8 MB (56.05%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     bool checkPerfectNumber(int num) {
         int sum=0;
+        int p=10000;
         for(int i=1;i<=num/2;i++){
+            if(i>=p) break;
             if(num%i==0){
                 sum+=i;
+                sum+=num/i;
+                p=num/i;
             }
         }
-        if(num!=sum) return false;
+        cout<<sum;
+        if(num!=sum/2) return false;
         return true;
     }
 };
