19a4bb940f6b1be7ccc0a94577db669034c7158b
Rahul Gupta <rahulgupta79777@gmail.com>
2025-08-25 19:09:23 +0530

    Time: 4 ms (55.11%), Space: 11.6 MB (71.59%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -4,16 +4,9 @@ public:
         vector<vector<int>> ans;
         vector<int> temp;
         vector<bool> ok(nums.size(),false);
+        sort(nums.begin(),nums.end());
         perm(nums,temp,ans,ok);
-        vector<vector<int>> ans1;
-        sort(ans.begin(),ans.end());
-        ans1.push_back(ans[0]);
-        for(int i=1;i<ans.size();i++){
-            if(ans[i]!=ans[i-1]){
-                ans1.push_back(ans[i]);
-            }
-        }
-        return ans1;
+        return ans;
     }
     void perm(vector<int>& nums,vector<int>& temp,vector<vector<int>>& ans,vector<bool>& ok){
         if(temp.size()==nums.size()){
@@ -22,13 +15,13 @@ public:
         }
 
         for(int i=0;i<nums.size();i++){
-            if(ok[i]!=true){
+            if(ok[i]==true) continue;
+            if(i>0&&nums[i]==nums[i-1]&&!ok[i-1]) continue;
                 temp.push_back(nums[i]);
                 ok[i]=true;
                 perm(nums,temp,ans,ok);
                 temp.pop_back();
                 ok[i]=false;
-            }
         }
     }
 };
