c2a876bca698e32d15310fb19d5d9bc0d519dd40
Rahul Gupta <rahulgupta79777@gmail.com>
2026-02-10 14:45:22 +0530

    Time: 416 ms (96.02%), Space: 132.1 MB (98.11%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -4,8 +4,9 @@ public:
         int ans=0;
         int even=0;
         int odd=0;
+        unordered_set<int> set;
         for(int i=0;i<nums.size();i++){
-            unordered_set<int> set;
+            set.clear();
             even=0;
             odd=0;
             if(nums.size()-i<=ans) break;
